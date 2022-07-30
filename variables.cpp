62c3ebdc5eec90525666e5c4329efc64db42a26d
“Tehreemathar” <tehreemathar17@gmail.com>
2022-07-30 15:42:53 +0500

    First cpp program

---
+++ variables.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    cout << "Once there was a man named Josh" << endl;
+    cout << "Josh was 80 years old" << endl;
+    cout << "He loved his name Josh," << endl;
+    cout << "but not being him 80" << endl;
+
+}
