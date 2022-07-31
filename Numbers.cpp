0e9ba72392062c1e6dc0dfa09896ca198e436b70
“Tehreemathar” <tehreemathar17@gmail.com>
2022-07-31 20:21:34 +0500

    Incrementation

---
+++ Numbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    int wnumber = 5;
+    double dnumber = 5.5;
+    
+    wnumber ++ ;
+    cout << wnumber << endl;
+    return 0;
+}
