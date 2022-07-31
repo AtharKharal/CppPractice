7272af0270f6f1537590810562235c6445f4de5f
“Tehreemathar” <tehreemathar17@gmail.com>
2022-07-31 15:15:39 +0500

    Strings

--- WorkingWithStrings.cpp
+++ WorkingWithStrings.cpp
@@ -6,8 +6,8 @@ using namespace std;
 int main ()
 {
    string str = "Tehreem Athar";
-   cout << "the size of the string is of " << str.length() << " bytes" << endl;
+   str [0] = 'H';
+   cout << str << endl;
 
-    
     return 0; 
 }
