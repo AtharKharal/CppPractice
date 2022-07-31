7ee54e0038fed76a597fe2b1fd4f0162837ba2c1
“Tehreemathar” <tehreemathar17@gmail.com>
2022-07-31 15:37:48 +0500

    substrFunc

--- WorkingWithStrings.cpp
+++ WorkingWithStrings.cpp
@@ -1,13 +1,11 @@
 #include <iostream>
-#include <string>
 
 using namespace std;
 
 int main ()
 {
    string str = "Tehreem Athar";
-   str [0] = 'H';
-   cout << str << endl;
+   cout << str.substr(8, 3) << endl;
 
     return 0; 
 }
