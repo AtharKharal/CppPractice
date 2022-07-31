9fe5708883c885ec0aad2800e0649e8c0721b3c0
“Tehreemathar” <tehreemathar17@gmail.com>
2022-07-31 11:24:58 +0500

    TheStr.LenthFunc

---
+++ WorkingWithStrings.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main ()
+{
+   string str = "Tehreem Athar";
+   cout << "the size of the string is of " << str.length() << " bytes" << endl;
+
+    
+    return 0; 
+}
