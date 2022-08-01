6e9595310b5c9dfdf9c494654f3dae2db2d3bd58
“Tehreemathar” <tehreemathar17@gmail.com>
2022-08-01 15:50:39 +0500

    CreatedMadlibs

--- BuildingACalculator.cpp
+++ BuildingACalculator.cpp
@@ -2,17 +2,14 @@
 #include <cmath>
 
 using namespace std;
-
 int main()
 {
     int num1, num2;
     cout << "Enter the first number: ";
     cin >> num1;
 
-    cout << "Enter the second number: ";
+    cout << "Enter the second number : ";
     cin >> num2;
 
-    cout << num1 + num2 << endl;
-
-    return 0;
+    cout << "The answer is " <<  num1 + num2 << endl;
 }
