0757c8b6ea4db1c5d5530acfcff0cb0051c3296b
“Tehreemathar” <tehreemathar17@gmail.com>
2022-08-02 14:46:48 +0500

    UsingFunctions

---
+++ Functions.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std; 
+
+void sayHi(string name, int age)
+{
+    cout << "Hello " << name << " You're " << age << endl; 
+
+}
+
+int main()
+{
+  
+   sayHi("Tehreem", 16); 
+
+    return 0;
+}
