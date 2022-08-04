422efa14a826cf867572c12c216dc6655e4b9fbd
“Tehreemathar” <tehreemathar17@gmail.com>
2022-08-04 15:31:34 +0500

    ElseIfAndNegation

--- IfStatement.cpp
+++ IfStatement.cpp
@@ -10,6 +10,14 @@ int main()
     {
         cout << "You are a tall Female" << endl; 
     }
+    else if (isFemale && !isTall)
+    {
+    cout << "You're a short Female" << endl; 
+    }
+    else if(!isFemale && isTall)
+    {
+        cout << "You're tall but not a female" <<endl;
+    }
     else
     {
         cout << "You are a not a Tall Female" << endl;
