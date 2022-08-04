c9e1cbe6bac8965155e09eaf3f0a2ef90ac51fcb
“Tehreemathar” <tehreemathar17@gmail.com>
2022-08-04 11:55:39 +0500

    &&Operators

--- IfStatement.cpp
+++ IfStatement.cpp
@@ -5,12 +5,13 @@ using namespace std;
 int main()
 {
     bool isFemale = false;
-    if (isFemale)
+    bool isTall = true;
+    if (isFemale && isTall)
     {
-        cout << "You are a Female" << endl; 
+        cout << "You are a tall Female" << endl; 
     }
     else
     {
-        cout << "You are a Male" << endl;
+        cout << "You are a not a Tall Female" << endl;
     }
 }
