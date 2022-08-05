089d23101ccc6123e12834571017c01e1b3a58b7
“Tehreemathar” <tehreemathar17@gmail.com>
2022-08-05 15:21:25 +0500

    practingIFStatements

---
+++ practice.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int getmax(int num1, int num2, int num3){
+    int result;
+    
+    if (num1 >= num2 && num1 >= num3){
+        result = num1;
+    } 
+    else if (num2 >= num1 && num2 >= num3){
+        result = num2;
+    }
+    else {
+        result = num3;
+    }
+    return result;
+}
+
+int main()
+{
+    cout << getmax (9, 6, 7) << endl;
+    return 0;   
+}
