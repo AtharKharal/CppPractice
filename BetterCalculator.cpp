1f92e08a326f4a5a1c08c96e465e02856de7b89d
“Tehreemathar” <tehreemathar17@gmail.com>
2022-08-05 22:27:13 +0500

    BetterCalculator

---
+++ BetterCalculator.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    int num1, num2;
+    char opr;
+
+    cout << "Enter the first number: " << endl;
+    cin >> num1; 
+    cout << "Enter an operator: " << endl;
+    cin >> opr;
+    cout << "Enter the second number: " << endl;
+    cin >> num2;
+    int result; 
+
+    if (opr == '+'){
+        result = num1 + num2;
+    } else if (opr == '-'){
+        result = num1 - num2;
+    } else if (opr == '/'){
+        result == num1 / num2;
+    } else if (opr == '*'){
+        result = num1 * num2;
+    } else {
+        cout << "Invalid operator" << endl;
+    }
+    cout << result << endl;
+    
+
+}
