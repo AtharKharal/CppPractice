75e1a213c2b05ef7c214d63aacd546bde9ff1751
“Tehreemathar” <tehreemathar17@gmail.com>
2022-08-08 16:06:43 +0500

    ForLoops

---
+++ ForLoops.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    int nums [] {2, 6, 8, 3, 5};
+    for(int i=0; i < 5; i++){
+        cout << nums [i] << endl;
+    }
+    return 0;
+}
