756a844833810c0985a7fed732cffedab6c8bb67
“Tehreemathar” <tehreemathar17@gmail.com>
2022-08-09 15:57:17 +0500

    2Darrays&nestedLoops

---
+++ ArraysAndNestedLoops.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    int NumberGrid[4][2] = {
+        {4, 0},
+        {8, 9},
+        {6, 4},
+        {1, 0}
+    };
+    cout << NumberGrid[0][0] << endl;
+    
+    return 0;
+    
+}
