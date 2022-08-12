e1b50e5eecf2d5617c71adf908af1ccf468cd863
“Tehreemathar” <tehreemathar17@gmail.com>
2022-08-12 13:02:32 +0500

    Shortercode

--- ConstructorFunctions.cpp
+++ ConstructorFunctions.cpp
@@ -6,24 +6,21 @@ class Animals {
         string name;
         string breed;
         int age;
-        Animals (string name){
-            cout << name << endl; 
+        Animals (string aName, string aBreed, int aAge){
+            name = aName;
+            breed = aBreed;
+            age = aAge;
+            
         }
     
 };
 
 int main ()
 {
-    Animals dog1("Oscar");
-    dog1.name = "Oscar";
-    dog1.breed = "Golden Retriever";
-    dog1.age = 9;
-
-    Animals dog2("Charlie");
-    dog2.name = "Charlie";
-    dog2.breed = "Golden Shepherd";
-    dog2.age = 7;
-
+    Animals dog1("Oscar", "Golden Retriever", 9);
+    Animals dog2("Charlie", "Golden Shepherd", 7);
+    
+    cout << dog1.name << endl;
     return 0;
 
 }
